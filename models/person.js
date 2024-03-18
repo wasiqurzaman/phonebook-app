80ecf98f1a5ef3aed3e03838ec34fb4f7db3b8e8
Wasiqur Zaman <wasiqurzaman@gmail.com>
2024-03-19 01:30:25 +0530

    uibuild

--- models/person.js
+++ models/person.js
@@ -17,7 +17,17 @@ const personSchema = new mongoose.Schema({
     minLength: 3,
     required: true,
   },
-  number: String,
+  number: {
+    type: String,
+    minLength: 8,
+    validate: {
+      validator: (v) => {
+        return /\d{2}-\d{5}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number.`
+    },
+    required: [true, "User phone number required."],
+  },
 });
 
 personSchema.set('toJSON', {
